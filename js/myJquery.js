d9baa0f81aa178bc3fdb11126c811dbaf0207cda
RahulsHubb <dewalsuccess@gmail.com>
2021-12-07 15:29:06 +0530

    rahul

---
+++ js/myJquery.js
@@ -0,0 +1,7 @@
+$(".after").click(function() {
+    $(".after i").toggleClass("fa-chevron-up");
+})
+
+$(".after-garage").click(function() {
+    $(".after-garage i").toggleClass("fa-chevron-up");
+})
